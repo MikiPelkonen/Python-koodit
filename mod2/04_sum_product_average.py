63a18bc58c884fe260079e3e708f3267fdbf9da3
Hovinarri <miki.pelkonen@gmail.com>
2025-08-20 02:45:34 +0300

    Fix comments

--- mod2/04_sum_product_average.py
+++ mod2/04_sum_product_average.py
@@ -8,25 +8,25 @@ sum: int = 0
 product: int = 1
 average: float = 0.0
 
-# Iterate prompt indexes
+# Iterate prompt indexes.
 for id in PROMPT_INDEXES:
-    # Loop until user inputs an integer value
+    # Loop until user inputs an integer value.
     while True:
         try:
-            # Prompt user for an integer
+            # Prompt user for an integer.
             prompt_input: int = int(input(f"{'Enter the ' + id + ' integer:':30s}"))
-            # Add the prompted integer value to the total sum value
+            # Add the prompted integer value to the total sum value.
             sum += prompt_input
-            # Multiply the current product value by the prompted integer value
+            # Multiply the current product value by the prompted integer value.
             product *= prompt_input
             break
-        # Catch non integer inputs
+        # Catch non integer inputs.
         except ValueError:
             print(INVALID_INTEGER_ERROR)
 
 # Calculate the average.
 average: float = sum / INTEGER_COUNT
-# Print the results
+# Print the results.
 print("\nRESULTS:")
 print(f"{'The sum of the numbers: ':30s}{sum}")
 print(f"{'The product of the numbers: ':30s}{product}")
