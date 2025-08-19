c14ee656ff371e515591dc2e2932d32df3b93c49
Hovinarri <miki.pelkonen@gmail.com>
2025-08-19 11:33:26 +0300

    Add comments

--- mod2/02_circle.py
+++ mod2/02_circle.py
@@ -1,5 +1,7 @@
+# Import pi from math library
 from math import pi
 
+# Loop until user inputs a number
 while True:
     try:
         radius = float(input("Enter circle radius: "))
@@ -7,5 +9,7 @@ while True:
     except ValueError:
         print("Invalid input! Please enter integer or floating point number")
 
+# Calculate circle area
 area_circle = pi * (radius**2)
+
 print(f"Area of circle with radius: {radius} is: {area_circle}")
