42e8738705524b7f6c6abc9a48a034d8d4fac075
Hovinarri <miki.pelkonen@gmail.com>
2025-08-19 14:22:48 +0300

    Fix namings to match the assignment on circle

--- mod2/02_circle.py
+++ mod2/02_circle.py
@@ -4,12 +4,12 @@ from math import pi
 # Loop until user inputs a number
 while True:
     try:
-        radius = float(input("Enter circle radius: "))
+        radius = float(input("Enter the radius of the circle: "))
         break
     except ValueError:
-        print("Invalid input! Please enter integer or floating point number")
+        print("Invalid input! Please enter an integer or a floating point number.")
 
 # Calculate circle area
-area_circle = pi * (radius**2)
+area = pi * (radius**2)
 
-print(f"Area of circle with radius: {radius} is: {area_circle}")
+print(f"The area of the circle is {area}")
