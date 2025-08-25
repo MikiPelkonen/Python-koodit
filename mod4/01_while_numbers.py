c2b74b2d3c834180fb5d2ed99ca0570d74f54fd6
Hovinarri <miki.pelkonen@gmail.com>
2025-08-25 15:23:29 +0300

    Add assignment 01 mod 4

---
+++ mod4/01_while_numbers.py
@@ -0,0 +1,13 @@
+# Constants.
+LAST_NUMBER = 1000
+
+# Variable for number/index
+index = 1
+
+# Loop while index is less than last number (1000).
+while index < LAST_NUMBER:
+    # Print if index is divisible by 3.
+    if index % 3 == 0:
+        print(index)
+    # Increment index.
+    index += 1
