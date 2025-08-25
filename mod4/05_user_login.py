637317b270dd115934b5e8c0cc112d49b8848dba
Hovinarri <miki.pelkonen@gmail.com>
2025-08-25 21:09:58 +0300

    Add solution to assignment 05 on module 4

---
+++ mod4/05_user_login.py
@@ -0,0 +1,36 @@
+# Constants.
+USERNAME_PROMPT: str = "Enter username: "
+PASSWORD_PROMPT: str = "Enter password: "
+USERNAME: str = "python"
+PASSWORD: str = "rules"
+MAX_TRIES: int = 5
+INVALID_CREDS_MSG: str = "Incorrect username or password. Please try again."
+ACCESS_DENIED_MSG: str = "Access denied"
+WELCOME_MSG: str = "Welcome"
+
+# Variable for try count.
+try_count: int = 1
+
+# Loop while try count is less than or equal to max tries count.
+while try_count <= MAX_TRIES:
+    input_username = input(USERNAME_PROMPT)
+    input_password = input(PASSWORD_PROMPT)
+
+    # Break loop early if username and password match the constants.
+    if input_username == USERNAME and input_password == PASSWORD:
+        # Print welcome message.
+        print(WELCOME_MSG)
+        break
+
+    # If try count less than max tries:
+    # -> print invalid credentials message.
+    if try_count < MAX_TRIES:
+        print(INVALID_CREDS_MSG)
+    else:
+        # Else print access denied on failed last try
+        # and break early.
+        print(ACCESS_DENIED_MSG)
+        break
+
+    # Increment try count.
+    try_count += 1
