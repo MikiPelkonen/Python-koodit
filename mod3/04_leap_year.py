ca4965dfa8d59b5d26579ff71deb99818b2786fa
Hovinarri <miki.pelkonen@gmail.com>
2025-08-25 14:59:07 +0300

    Add assignment 04 mod 3

---
+++ mod3/04_leap_year.py
@@ -0,0 +1,18 @@
+# Constants.
+YEAR_PROMPT: str = "Enter a year: "
+INVALID_NUMBER_ERROR = "Please input a valid number."
+
+# Prompt user for a year.
+# Loop until user inputs a valid number (integer).
+while True:
+    try:
+        year = int(input(YEAR_PROMPT))
+        break
+    except ValueError:
+        print(INVALID_NUMBER_ERROR)
+
+# Evalutate if year is divisible by 4 and not divisible by 100 or year is divisible by 400.
+is_leap_year: bool = (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+# Print results.
+print(f"{year} {'is' if is_leap_year else 'is not'} a leap year.")
