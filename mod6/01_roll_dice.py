8bf9bf877110719be941b1b8972a8fe80cf5a00f
Hovinarri <miki.pelkonen@gmail.com>
2025-08-29 18:41:39 +0300

    Add file level docstring

--- mod6/01_roll_dice.py
+++ mod6/01_roll_dice.py
@@ -1,3 +1,24 @@
+"""
+File: 01_roll_dice.py
+
+Description:
+    Simulates rolling a six-sided die until a six is rolled.
+    Prints each roll value.
+
+Functions:
+    roll_dice():
+        Simulates rolling a six-sided die and returns result.
+
+Example:
+    >>> roll_dice()
+    4
+    >>> roll_dice()
+    6
+
+Author: Miki Pelkonen
+Date: 2025-08-29
+"""
+
 # Import random for random number generation.
 import random
 
@@ -14,7 +35,6 @@ def roll_dice():
 
 
 # Main program.
-# Int variable to hold dice roll return value.
 dice_roll_result: int = 0
 # Loop until dice roll return value is number 6.
 while dice_roll_result != 6:
