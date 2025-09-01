0b75828e9d9d5e993ea6ef576e7a4358be260693
Hovinarri <miki.pelkonen@gmail.com>
2025-09-01 15:54:31 +0300

    Cleanup and add method to check roomtype by coords to world

--- extras/text_adventure.py
+++ extras/text_adventure.py
@@ -1,22 +1,50 @@
 from enum import Enum
 
 
-# Classes.
-class Player:
-    def __init__(self, name) -> None:
-        self.name = name
-        self.level = 1
-
-
+# Enums.
 class GameSize(Enum):
     SMALL = 4
     MEDIUM = 6
     LARGE = 8
 
 
+class RoomType(Enum):
+    EMPTY = 0
+    ENTRANCE = 1
+    LIBRARY = 2
+    KITCHEN = 3
+
+
+# Classes.
+class Coordinate:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"X: {self.x} Y: {self.y}"
+
+    @property
+    def get_x(self) -> int:
+        return self.x
+
+    @property
+    def get_y(self) -> int:
+        return self.y
+
+
+class Player:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.level = 1
+
+
 class World:
     def __init__(self, rooms) -> None:
         self.rooms = rooms
+
+    def get_room_type(self, coords: Coordinate) -> RoomType:
+        return self.rooms[coords.get_x][coords.get_y].room_type
 
 
 class WorldBuilder:
@@ -36,22 +64,6 @@ class WorldBuilder:
             RoomType.KITCHEN, Coordinate(length // 2, length - 1)
         )
         return World(rooms)
-
-
-class Coordinate:
-    def __init__(self, x: int, y: int) -> None:
-        self.x = x
-        self.y = y
-
-    def __repr__(self) -> str:
-        return f"X: {self.x} Y: {self.y}"
-
-
-class RoomType(Enum):
-    EMPTY = 0
-    ENTRANCE = 1
-    LIBRARY = 2
-    KITCHEN = 3
 
 
 class Room:
@@ -76,3 +88,5 @@ world_builder = WorldBuilder(GameSize.SMALL)
 world = world_builder.build_world()
 for x in world.rooms:
     print(x)
+
+print(world.get_room_type(Coordinate(0, 0)))
