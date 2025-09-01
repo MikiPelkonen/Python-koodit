ac63562f6595065bd7b3e9bbaa2bb7c56260b50b
Hovinarri <miki.pelkonen@gmail.com>
2025-09-01 15:45:40 +0300

    Add world, room and builder to text adventure WIP

---
+++ extras/text_adventure.py
@@ -0,0 +1,78 @@
+from enum import Enum
+
+
+# Classes.
+class Player:
+    def __init__(self, name) -> None:
+        self.name = name
+        self.level = 1
+
+
+class GameSize(Enum):
+    SMALL = 4
+    MEDIUM = 6
+    LARGE = 8
+
+
+class World:
+    def __init__(self, rooms) -> None:
+        self.rooms = rooms
+
+
+class WorldBuilder:
+    def __init__(self, size: GameSize) -> None:
+        self.game_size = size
+
+    def build_world(self) -> World:
+        length = self.game_size.value
+        rooms = [
+            [Room(RoomType.EMPTY, Coordinate(row, col)) for col in range(length)]
+            for row in range(length)
+        ]
+        # Place entrance
+        rooms[0][0] = Room(RoomType.ENTRANCE, Coordinate(0, 0))
+        # Place kitchen
+        rooms[length // 2][length - 1] = Room(
+            RoomType.KITCHEN, Coordinate(length // 2, length - 1)
+        )
+        return World(rooms)
+
+
+class Coordinate:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def __repr__(self) -> str:
+        return f"X: {self.x} Y: {self.y}"
+
+
+class RoomType(Enum):
+    EMPTY = 0
+    ENTRANCE = 1
+    LIBRARY = 2
+    KITCHEN = 3
+
+
+class Room:
+    def __init__(self, room_type: RoomType, coords: Coordinate) -> None:
+        self.room_type = room_type
+        self.coords = coords
+
+    @property
+    def get_room_type(self) -> RoomType:
+        return self.room_type
+
+    @property
+    def get_coords(self) -> Coordinate:
+        return self.coords
+
+    def __repr__(self) -> str:
+        return f"{self.room_type}({self.coords})"
+
+
+# Main program.
+world_builder = WorldBuilder(GameSize.SMALL)
+world = world_builder.build_world()
+for x in world.rooms:
+    print(x)
