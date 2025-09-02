b7c56957b820da953a30593ca645f0c7e8105a61
Hovinarri <miki.pelkonen@gmail.com>
2025-09-02 09:55:58 +0300

    Add solution to assignment 02 of module 7

---
+++ mod7/02_name_set.py
@@ -0,0 +1,56 @@
+"""
+File: 02_name_set.py
+
+Description:
+    A simple program that keeps prompting user for names and collects them in a set.
+    Prints "New name" on unique keys and "Existing name" ond duplicate keys.
+    Inputting empty string stops prompting and prints the results.
+
+Functions:
+    prompt_name_for_set() -> set[str]:
+
+Example:
+    >>> Enter a name: miki
+    New name
+    >>> Enter a name: miki
+    Existing name
+    >>> Enter a name:
+    miki
+
+Author: Miki Pelkonen
+Date: 2025-09-02
+"""
+
+# Constants.
+NAME_PROMPT: str = "Enter a name: "
+NAME_RESULTS: tuple[str, str] = ("New name", "Existing name")
+
+
+# Functions.
+def prompt_name_for_set() -> set[str]:
+    """
+    Keeps prompting user for names until an empty string is passed.
+
+    Returns:
+        set[str]: The names in a set.
+    """
+    result_set: set[str] = set()
+    while True:
+        user_input = input(NAME_PROMPT)
+        if not user_input:
+            break
+        try:
+            if user_input in result_set:
+                raise KeyError
+            result_set.add(user_input)
+            print(NAME_RESULTS[0])
+        except KeyError:
+            print(NAME_RESULTS[1])
+
+    return result_set
+
+
+# Main program.
+names = prompt_name_for_set()
+for name in names:
+    print(name)
