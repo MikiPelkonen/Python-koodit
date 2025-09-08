26e40145224381bd8791211430044e1e32c76bbe
Hovinarri <miki.pelkonen@gmail.com>
2025-09-08 14:00:16 +0300

    Add extra pakkauslista

---
+++ extras/pakkauslista.py
@@ -0,0 +1,17 @@
+ITEM_PROMPT: str = "Anna esine (tyhjä lopettaa): "
+RESULT_HEADING: str = "\nAnnetut esineet:"
+
+item_set = set()
+
+while True:
+    user_input = input(ITEM_PROMPT)
+    if not user_input:
+        break
+    if user_input not in item_set:
+        item_set.add(user_input)
+    else:
+        print(f"{user_input} löytyy jo!")
+
+print(RESULT_HEADING)
+for idx, item in enumerate(item_set):
+    print(f"{idx + 1}. {item.capitalize()}")
