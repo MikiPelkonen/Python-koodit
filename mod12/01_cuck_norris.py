6916cee6eede631521c92b6faa191ef3345ebc6c
Hovinarri <miki.pelkonen@gmail.com>
2025-10-24 13:10:54 +0300

    Add assignment 01 mod12

--- mod12/01_cuck_norris.py
+++ mod12/01_cuck_norris.py
@@ -5,8 +5,10 @@ REQUEST_URL: str = "https://api.chucknorris.io/jokes/random"
 try:
     random_joke_request = requests.get(REQUEST_URL)
     if random_joke_request.status_code != 200:
-        raise requests.RequestException
+        raise requests.RequestException(
+            f"Invalid status code: {random_joke_request.status_code}"
+        )
     random_joke_json = random_joke_request.json()
     print(random_joke_json["value"])
-except requests.RequestException as e:
+except requests.exceptions.RequestException as e:
     print(f"Error fetching a joke: {e}")
