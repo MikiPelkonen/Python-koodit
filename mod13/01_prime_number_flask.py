a95bcabd8f6fb3d95ebd9fc80c05a397c21eb9ed
Hovinarri <miki.pelkonen@gmail.com>
2025-11-11 19:51:58 +0200

    Fix assignment 01mod13 wip

--- mod13/01_prime_number_flask.py
+++ mod13/01_prime_number_flask.py
@@ -1,16 +1,21 @@
 from math import isqrt
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def is_prime_number(number) -> bool:
     if number <= 1:
         return False
-    elif number % 2 == 0:
+
+    if number == 2:
         return True
-    else:
-        for i in range(2, isqrt(number) + 1):
-            if number % i == 0:
-                return False
+
+    if number % 2 == 0:
+        return False
+
+    for i in range(2, isqrt(number) + 1):
+        if number % i == 0:
+            return False
+
     return True
 
 
@@ -19,5 +24,8 @@ app = Flask(__name__)
 
 @app.route("/alkuluku/<int:number>", methods=["GET"])
 def prime_route(number):
-    json = {"Number": number, "isPrime": is_prime_number(number)}
-    return json
+    result = {"number": number, "isPrime": is_prime_number(number)}
+    return jsonify(result), 200
+
+
+app.run(host="127.0.0.1", port=5000, debug=True)
